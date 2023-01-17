c579f3e45aaf8a9d495213f0107d91a622621b5c
vanesaespin <vespin@ieshlanz.es>
2023-01-17 17:22:31 +0100

    a

--- Javascript/U5-Eventos y formularios/Ejercicios/Ejercicio7Antonio/Ejercicio7.js
+++ Javascript/U5-Eventos y formularios/Ejercicios/Ejercicio7Antonio/Ejercicio7.js
@@ -3,19 +3,19 @@ function coordenadas(event) {
     let y = event.clientY;
     let anchura = window.innerWidth;
     let altura = window.innerHeight;
-    if(x>altura/2 && y>anchura/2){
+    if(x>anchura/2 && y>altura/2){
 
         document.getElementById("demo").innerHTML = "Derecha Abajo";
 
-    }else if(x>altura/2 && y<anchura/2){
+    }else if(x>anchura/2 && y<altura/2){
 
         document.getElementById("demo").innerHTML = "Derecha Arriba";
 
-    }else if(x<altura/2 && y<anchura/2){
+    }else if(x<anchura/2 && y<altura/2){
 
         document.getElementById("demo").innerHTML = "Izquierda Arriba";
 
-    }else if(x<altura/2 && y>anchura/2){
+    }else if(x<anchura/2 && y>altura/2){
 
         document.getElementById("demo").innerHTML = "Izquierda Abajo";
 
