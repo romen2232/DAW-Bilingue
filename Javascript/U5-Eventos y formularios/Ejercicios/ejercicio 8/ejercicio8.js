25056bac408627a75dc722cb73dd68c66c7cb20b
vanesaespin <vespin@ieshlanz.es>
2023-01-17 17:43:05 +0100

    a

--- Javascript/U5-Eventos y formularios/Ejercicios/ejercicio 8/ejercicio8.js
+++ Javascript/U5-Eventos y formularios/Ejercicios/ejercicio 8/ejercicio8.js
@@ -1,6 +1,6 @@
 var textarea = document.getElementById("textarea");
 var caracter = 3; 
-
+    /**intentar no usar esta forma de crear eventos */
     textarea.onkeypress = function(evento) {
         if (evento.target.value.length >= caracter) {
             return false;
