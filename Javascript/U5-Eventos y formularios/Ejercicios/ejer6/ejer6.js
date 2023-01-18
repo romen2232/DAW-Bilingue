c8334be745913e239238ac89c5386490570c1ccc
vanesaespin <vespin@ieshlanz.es>
2023-01-18 09:41:47 +0100

    ej6 modificado

--- Javascript/U5-Eventos y formularios/Ejercicios/ejer6/ejer6.js
+++ Javascript/U5-Eventos y formularios/Ejercicios/ejer6/ejer6.js
@@ -1,18 +1,26 @@
-var contenedor = document.querySelector(".gallery-container");
 
-/*contenedor.addEventListener('click', e => {
-    if(e.target.classList.contains('gallery-img')){
+
+/* 
+* PEQUEÑO CAMBIO AL EJEMPLO DE HUGO PARA QUE FUNCIONE: 
+* INICIALIZAMOS LAS IMÁGENES A 200 DE ANCHO
+*/
+
+//Guardamos contenedor
+var contenedor = document.querySelector(".gallery-container");
+//Cogemos las imágenes del documento y le ponemos tamaño
+const imagenes=document.querySelectorAll(".gallery-img");
+for (imagen of imagenes){
+  imagen.style.width="200px";
+}
+ 
+//Ya podemos hacer las comparaciones con el estilo
+  contenedor.addEventListener('click', e => {
+    
         if (e.target.style.width  == "200px"){
             e.target.style.width  = "500px";
         }else{
-            console.log("entro en el else")
             e.target.style.width  = "200px";
         }
-    }
-  });*/
-
-  contenedor.addEventListener('click', e => {
-    if(e.target.classList.contains('gallery-img')){
-      e.target.style.width="400px";
-    }
+    
   });
+    
